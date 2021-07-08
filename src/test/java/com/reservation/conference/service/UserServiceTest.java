4aff7e03a2befc8597928272c4f2a192b7dba6be
minkeun <minkeun428@gmail.com>
2021-07-08 23:00:51 +0900

    test : 주석 설명 추가

--- src/test/java/com/reservation/conference/service/UserServiceTest.java
+++ src/test/java/com/reservation/conference/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.reservation.conference.service;
 
+import com.reservation.conference.dto.UserLoginDto;
 import com.reservation.conference.utils.SecurityUtil;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -7,46 +8,51 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 /**
  * @SpringBootTest
  * 애플리케이션의 설정, 모든 Bean을 모두 로드하기 때문에 전체적인 Flow 테스트 가능
  * 하지만, 모두 로드하기 때문에 시간이 오래 걸리고 무거움
- * */
+ **/
 @SpringBootTest
-class UserLoginServiceTest {
+class UserServiceTest {
 
+    /**
+     * JUnit에서 생성자나 lombok방식으로 DI가 안되는 이유는
+     * JUnit이 생성자에 다른 의존성을 주입하려고 먼저 개입하기 때문이다.
+     * */
     @Autowired
-    UserLoginService userLoginService;
+    UserService userService;
 
 
     @Test
     @DisplayName("로그인 성공 테스트")
     void loginCheckSuccess() throws Exception {
         //given
-        Long userId = 1L;
+        String userId = "testUser";
         String userPassword = "1234";
         String testEncryptPassword = SecurityUtil.encryptPassword(userPassword); // 직접 암호화
 
         //when
-        String userEncryptPassword = userLoginService.login(userId, userPassword);
+        UserLoginDto userLoginDto = userService.login(userId, userPassword);
 
         //then
-        Assertions.assertEquals(testEncryptPassword, userEncryptPassword);
+        Assertions.assertEquals(testEncryptPassword, userLoginDto.getPassword());
     }
 
     @Test
     @DisplayName("로그인 실패 테스트")
     void loginCheckFail() throws Exception {
         //given
-        Long userId = 1L;
+        String userId = "testUser";
         String userPassword = "1234";
         String testEncryptPassword = SecurityUtil.encryptPassword("5678");  //틀린 비밀번호 암호화
 
         //when
-        String userEncryptPassword = userLoginService.login(userId, userPassword);
+        UserLoginDto userLoginDto = userService.login(userId, userPassword);
 
         //then
-        Assertions.assertNotEquals(testEncryptPassword, userEncryptPassword);
+        Assertions.assertNotEquals(testEncryptPassword, userLoginDto.getPassword());
     }
 
 }
