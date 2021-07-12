ba9cfbbb670c5f2ed2377fbd5f5f3eebd34ece01
heo-ella <yeseul2972@gmail.com>
2021-07-12 23:52:59 +0900

    feat : 유저 회원가입 mapper 추가

--- src/main/java/com/reservation/conference/mapper/UserMapper.java
+++ src/main/java/com/reservation/conference/mapper/UserMapper.java
@@ -1,11 +1,15 @@
 package com.reservation.conference.mapper;
 
 
+import com.reservation.conference.dto.UserJoinDto;
 import com.reservation.conference.dto.UserLoginDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface UserMapper {
 
     UserLoginDto findUserByIdAndPassword(String id, String password);
+
+    void insertUser(UserJoinDto userJoinDto);
 }
