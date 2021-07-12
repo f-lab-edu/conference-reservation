d9a204ceb45aca62bd11f2890fd873fb8d52eeae
heo-ella <yeseul2972@gmail.com>
2021-07-12 23:51:57 +0900

    chore : Id 변수 자료형 및 소문자로 변경

--- src/main/java/com/reservation/conference/dto/UserJoinDto.java
+++ src/main/java/com/reservation/conference/dto/UserJoinDto.java
@@ -8,20 +8,20 @@ import lombok.Getter;
 : 자동으로 접근자 메소드를 생성한다.
 */
 @Getter
-public class UserJoinDTO {
-    private Long Id; // 자동으로 생성하는 systemId
-    private String userName;
+public class UserJoinDto {
+    private String id;
     private String password;
+    private String userName;
 
     /*
     @Builder
     : setter 메소드가 없는 객체를 생성하여 변경 불가능하게 한다.
     */
     @Builder
-    public UserJoinDTO(Long Id, String userName, String password){
-        this.Id  = Id;
-        this.userName = userName;
+    public UserJoinDto(String id, String password, String userName){
+        this.id  = id;
         this.password = password;
+        this.userName = userName;
     }
 
 }
