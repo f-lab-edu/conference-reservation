11c391f9c1147e19d164d86469a86bc9dd13dc8c
minkeun <minkeun428@gmail.com>
2021-07-23 00:06:31 +0900

    modify : CorpController 생성
    추후 API개발을 진행한다.

--- src/main/java/com/reservation/conference/controller/CorpController.java
+++ src/main/java/com/reservation/conference/controller/CorpController.java
@@ -13,9 +13,14 @@ import static com.reservation.conference.utils.HttpResponses.RESPONSE_OK;
 @RequestMapping("/corp")
 public class CorpController {
 
+    /**
+     *
+     * 해당 API는 아직 기능구현이 되지 않았습니다.
+     *
     @PostMapping("/join")
     public ResponseEntity join(@RequestBody CorpJoinDto corpJoinDto) {
 
         return RESPONSE_OK;
     }
+     */
 }
