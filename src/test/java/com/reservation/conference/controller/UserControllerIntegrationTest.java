8c22b7b35b12164b4add358f9f87bb9af46d9c35
minkeun <minkeun428@gmail.com>
2021-07-23 16:25:19 +0900

    test : UserControllerIntegrationTest
    port 추가

--- src/test/java/com/reservation/conference/controller/UserControllerIntegrationTest.java
+++ src/test/java/com/reservation/conference/controller/UserControllerIntegrationTest.java
@@ -1,17 +1,21 @@
 package com.reservation.conference.controller;
 
 import com.reservation.conference.dto.UserLoginDto;
+import com.reservation.conference.utils.HttpResponses;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserControllerIntegrationTest {
+
+    @LocalServerPort
+    private int port;
 
     @Autowired
     private TestRestTemplate testRestTemplate;
@@ -19,12 +23,13 @@ public class UserControllerIntegrationTest {
 
     @Test
     public void loginUserIntegrationTest() {
+        final String url = "http://localhost:" + port + "/users/login";
         UserLoginDto user = new UserLoginDto();
         user.setId("testId2");
         user.setPassword("12345");
 
-        ResponseEntity result = testRestTemplate.postForObject("/users/login", user, ResponseEntity.class);
-        Assertions.assertEquals(HttpStatus.OK, result.getStatusCode());
+        ResponseEntity result = testRestTemplate.postForObject(url, user, ResponseEntity.class);
+        Assertions.assertEquals(result.getStatusCode(), HttpResponses.RESPONSE_OK);
     }
 
 }
