24c4918b67fd3db7e4dcfbc9dbd18ed9ef714adb
minkeun <minkeun428@gmail.com>
2021-07-23 17:59:31 +0900

    chore : gitignore 파일 추가
    - application.properties
    - application.yml

--- src/main/java/com/reservation/conference/controller/CorpController.java
+++ src/main/java/com/reservation/conference/controller/CorpController.java
@@ -1,13 +1,7 @@
 package com.reservation.conference.controller;
 
-import com.reservation.conference.dto.CorpJoinDto;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static com.reservation.conference.utils.HttpResponses.RESPONSE_OK;
 
 @RestController
 @RequestMapping("/corp")
