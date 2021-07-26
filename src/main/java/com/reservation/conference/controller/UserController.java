d600d3a06cefca2317fb17878f26fb6bd87f9e04
minkeun <minkeun428@gmail.com>
2021-07-26 19:53:10 +0900

    modify : 유저 로그인 세션 추가
    - UserController session 추가
    - UserLoginDto @Setter 추가

--- src/main/java/com/reservation/conference/controller/UserController.java
+++ src/main/java/com/reservation/conference/controller/UserController.java
@@ -4,10 +4,9 @@ import com.reservation.conference.dto.UserLoginDto;
 import com.reservation.conference.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 import static com.reservation.conference.utils.HttpResponses.RESPONSE_OK;
 import static com.reservation.conference.utils.HttpResponses.RESPONSE_UNAUTHORIZED;
@@ -22,15 +21,26 @@ public class UserController {
 
 
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody UserLoginDto userLoginDto) throws Exception {
+    public ResponseEntity login(@RequestBody UserLoginDto userLoginDto, HttpSession session) throws Exception {
 
         UserLoginDto userInfo = userService.login(userLoginDto.getId(), userLoginDto.getPassword());
 
         if(userInfo == null) {
             return RESPONSE_UNAUTHORIZED;
         }else {
+            //로그인 성공 시, 세션에 userInfo 저장
+            session.setAttribute("user", userInfo);
             return RESPONSE_OK;
         }
+    }
+
+    @GetMapping("/logout")
+    public ResponseEntity logout(HttpSession session) {
+        //"user" 세션만 삭제
+        //session.invalidate() -> 모든 세션 초기화
+        session.removeAttribute("user");
+
+        return RESPONSE_OK;
     }
 
 }
