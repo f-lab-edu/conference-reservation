cf7adb72b13784820e38a758cf65756c76e6bc6e
minkeun <minkeun428@gmail.com>
2021-08-08 16:04:58 +0900

    feat : 유저 로그인 리턴 dto 생성

---
+++ src/main/java/com/reservation/conference/user/dto/UserLoginResponseDto.java
@@ -0,0 +1,14 @@
+package com.reservation.conference.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UserLoginResponseDto {
+
+    private String id;
+    private String userName;
+    private String userEmail;
+
+}
